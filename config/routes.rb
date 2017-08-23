6affacaec4c3cdd3288db36fb5411d48c9585fc4
Song <mssongan840136@outlook.com>
2017-08-23 19:26:49 +0900

    Ruby versionを合わせる

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :question_part_twos
   resources :question_part_ones
   root 'top#index'
-
-  get '/manage', to: "manage#index"
+  get  '/manage',           to: "manage#index"
+  get  '/infomation_index', to: 'infomation#infomation_index'
+  get  '/reply_index',      to: 'reply#reply_index'
 end
