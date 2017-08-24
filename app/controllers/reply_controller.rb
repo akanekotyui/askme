36469873bf8540862a0eb2b26f1f17c742bb31ee
akanekotyui <a_roid_ba_sky@yahoo.co.jp>
2017-08-24 11:09:19 +0900

    変数で質問文を表示させました

--- app/controllers/reply_controller.rb
+++ app/controllers/reply_controller.rb
@@ -1,4 +1,9 @@
 class ReplyController < ApplicationController
   def reply_index
+    #これは文字列直打ち
+    @reply = 'how to get to' + 'Kinkakuji-temple'
+    @answer = '*goto行きの	（～系統）で乗って、（～方面）へ行き、（降りるべき駅）で降りてください。'
+
   end
+
 end
