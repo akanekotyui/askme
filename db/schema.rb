b604c6ddcead2a86231f9407b38e2976791ee9f7
Song <mssongan840136@outlook.com>
2017-08-24 14:43:05 +0900

    Type項目を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824005048) do
+ActiveRecord::Schema.define(version: 20170824053352) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "code"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170824005048) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "code"
+    t.string "type"
   end
 
   create_table "question_part_twos", force: :cascade do |t|
