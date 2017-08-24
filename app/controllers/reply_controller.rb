a259219b277f50276bf23fae271367ac98969810
akanekotyui <a_roid_ba_sky@yahoo.co.jp>
2017-08-24 19:43:11 +0900

    入力された質問文と日本語訳をつけました

--- app/controllers/reply_controller.rb
+++ app/controllers/reply_controller.rb
@@ -1,10 +1,4 @@
 class ReplyController < ApplicationController
   def reply_index
-    #質問文からq2_codeとcodeをとってくる
-    q1 = QuestionPartOne.where(id: param[q1_id])
-    q2 = QuestionPartTwo.where(id: param[q2_id])
-
-    #
   end
-
 end
