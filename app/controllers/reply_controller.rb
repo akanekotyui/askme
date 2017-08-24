e3996806b2c85da2c101dc014bc76403aee48c40
akanekotyui <a_roid_ba_sky@yahoo.co.jp>
2017-08-24 14:51:23 +0900

    akane commit

--- app/controllers/reply_controller.rb
+++ app/controllers/reply_controller.rb
@@ -1,8 +1,11 @@
 class ReplyController < ApplicationController
   def reply_index
     #これは文字列直打ち
-    @reply = 'how to get to' + 'Kinkakuji-temple'
-    @answer = '*goto行きの	（～系統）で乗って、（～方面）へ行き、（降りるべき駅）で降りてください。'
+  #  @reply = 'how to get to' + 'Kinkakuji-temple'
+  #  @answer = '*goto行きの	（～系統）で乗って、（～方面）へ行き、（降りるべき駅）で降りてください。'
+
+#@reply = QuestionPartOne.human_attribute_name(:en) + QuestionPartTwo.human_attribute_name(:en)
+@reply = QuestionPartOne.all
 
   end
 
--- db/migrate/001_create_questions.rb
+++ db/migrate/001_create_questions.rb
@@ -4,20 +4,20 @@ class CreateQuestions < ActiveRecord::Migration[5.1]
       t.string  :en
       t.string  :jp
       t.string  :zh
-      t.integer :q2_code
+      t.integer :q2_code #how toなら1
       t.string  :remark
 
       t.timestamps
     end
 
     create_table :question_part_twos do |t|
-      t.integer :code
+      t.integer :code #how to なら1, Q1のq2_codeと一緒
       t.string  :en
       t.string  :jp
       t.string  :zh
-      t.integer :a_code
+      t.integer :a_code #答え方
       t.string  :remark
-      
+
       t.timestamps
     end
   end
