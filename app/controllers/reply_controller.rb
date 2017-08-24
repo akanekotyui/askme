e14c45fd7a96e46f7f6ecec4acb790962c18efe3
Song <mssongan840136@outlook.com>
2017-08-25 07:32:08 +0900

    replyを編集

--- app/controllers/reply_controller.rb
+++ app/controllers/reply_controller.rb
@@ -1,10 +1,6 @@
 class ReplyController < ApplicationController
   def reply_index
-    #質問文からq2_codeとcodeをとってくる
-    q1 = QuestionPartOne.where(id: param[q1_id])
-    q2 = QuestionPartTwo.where(id: param[q2_id])
-
-    #
+    @q1 = QuestionPartOne.where(id: params[:q1_id])
+    @q2 = QuestionPartTwo.where(id: params[:q2_id])
   end
-
 end
