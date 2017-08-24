366ed0a04605d6449274c5b7fc603880337c2055
Komori <takuan.daisuki.ningen@gmail.com>
2017-08-24 15:32:13 +0900

    hello

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-QuestionPartOne.create(:en => 'Which bus', :jp => 'どのバスに', :zh => '哪辆巴士' :q2_code => 1, :remark => 'should I take to')
-QuestionPartOne.create(:en => 'Which railway', :jp => 'どのバス', :zh => '其中列车' :q2_code => 1, :remark => 'should I take to')
-QuestionPartOne.create(:en => 'Where', :jp => 'どこ', :zh => '哪里' :q2_code => 1, :remark => 'is')
-QuestionPartOne.create(:en => 'Please help me', :jp => '助けてください', :zh => '请帮帮我' :q2_code => 5, :remark => '')
-QuestionPartOne.create(:en => 'I\'m in trouble', :jp => '困ってます', :zh => '我有麻烦了' :q2_code => 6, :remark => '')
-QuestionPartOne.create(:en => 'Is ninja', :jp => '忍者は', :zh => '忍者' :q2_code => 3, :remark => '')
