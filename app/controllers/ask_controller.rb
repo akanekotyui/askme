12545291c81d80e8918058e5e38a1d9e6de2f0e1
Song <mssongan840136@outlook.com>
2017-08-25 10:30:08 +0900

    中国語対応

--- app/controllers/ask_controller.rb
+++ app/controllers/ask_controller.rb
@@ -2,5 +2,8 @@ class AskController < ApplicationController
   def index
     @q1 = QuestionPartOne.find(params[:q1_id])
     @q2s = QuestionPartTwo.where(code: @q1.q2_code)
+    if @la == "zh"
+      render "index_zh"
+    end
   end
 end
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,7 @@
 class TopController < ApplicationController
   def index
+    if @la == "zh"
+      render "index_zh"
+    end
   end
 end
