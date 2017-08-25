a8f2779f31f61643879f91aba079269ebc38462e
Song <mssongan840136@outlook.com>
2017-08-25 10:23:58 +0900

    tranを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824103910) do
+ActiveRecord::Schema.define(version: 20170825011806) do
 
   create_table "advises", force: :cascade do |t|
     t.string "en"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170824103910) do
     t.string "zh"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "tran"
   end
 
   create_table "question_part_ones", force: :cascade do |t|
@@ -39,7 +40,6 @@ ActiveRecord::Schema.define(version: 20170824103910) do
     t.datetime "updated_at", null: false
     t.integer "code"
     t.string "tab_type"
-    t.string "type"
   end
 
   create_table "question_part_twos", force: :cascade do |t|
