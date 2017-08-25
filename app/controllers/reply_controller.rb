a2d51c836652909236951ba2d14e5c5037558130
Song <mssongan840136@outlook.com>
2017-08-25 12:04:16 +0900

    中国語対応

--- app/controllers/reply_controller.rb
+++ app/controllers/reply_controller.rb
@@ -4,5 +4,8 @@ class ReplyController < ApplicationController
     @q2 = QuestionPartTwo.find(params[:q2_id])
     answer_code = (@q1.code.to_s + @q2.code.to_s + @q2.a_code.to_s).to_i
     @a = Answer.where(code: answer_code).first
+    if @la == "zh"
+      render "reply_index_zh"
+    end
   end
 end
